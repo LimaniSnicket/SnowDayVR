7cebe05217459d0b891730c8f9dc514a3a77a520
Vera Limani <vl979@nyu.edu>
2020-02-07 22:45:13 -0500

    Testing snowball destruction: spawning particles at center of mass and getting the final mass & diameter of snowball on destroy

---
+++ SnowballVR/Assets/Scripts/SnowballBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(SphereCollider))]
+public class SnowballBehavior : MonoBehaviour
+{
+    public GameObject HitParticle;
+    Rigidbody snowballBody { get => GetComponent<Rigidbody>(); }
+    SphereCollider snowballCollider { get => GetComponent<SphereCollider>(); }
+    float sizeFactor { get => Mathf.Floor(snowballBody.mass + snowballCollider.radius); }
+
+    private void Start()
+    {
+        snowballBody.mass = 0.001f;
+        transform.localScale = Vector3.one * 0.001f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            ScaleUp();
+        }
+
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void ScaleUp()
+    {
+        snowballBody.mass += Time.deltaTime;
+        transform.localScale += Vector3.one * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        Instantiate(HitParticle, snowballBody.centerOfMass, new Quaternion(0,0,0,0));
+        Debug.Log(sizeFactor);
+    }
+}
