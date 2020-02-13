5029b27105959b735eabeac510f79d943b9fb259
Vera Limani <vl979@nyu.edu>
2020-02-13 14:33:10 -0500

    Snowfall Particle Effect + Snow mounds that increase in size over time

---
+++ SnowballVR/Assets/Editor/BeepBeepEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(RoadwayTrafficBeepBeep))]
+public class BeepBeepEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        RoadwayTrafficBeepBeep b = (RoadwayTrafficBeepBeep)target;
+        GUILayout.Label("CARS BY FREQUENCY", EditorStyles.boldLabel);
+        if (b.CarsByFrequency.Count > 0 && b.CarsByFrequency != null)
+        {
+            foreach (var i in b.CarsByFrequency)
+            {
+                GUILayout.Label(i.Value.Count + " :" + i.Key.ToString());
+            }
+        }
+
+        if (RoadwayTrafficBeepBeep.CarsToSpawn != null && RoadwayTrafficBeepBeep.CarsToSpawn.Count > 0)
+        {
+            foreach (var c in RoadwayTrafficBeepBeep.CarsToSpawn)
+            {
+                GUILayout.Label(c.CarID);
+            }
+        }
+    }
+}
